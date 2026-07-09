e209a4a1f85fce87e12ee0adbec3ddd782f76c1b
FelipeMoreira <felipemmares@gmail.com>
2026-07-09 12:59:38 -0300

    criando o HitData do Dummy

---
+++ Scripts/Dummy/HitData.gd
@@ -0,0 +1,49 @@
+extends Resource
+class_name HitData
+
+# ============================
+# DANO
+# ============================
+
+@export var damage: int = 100
+
+# ============================
+# HITSTUN
+# Quantos frames o oponente fica
+# travado após ser atingido.
+# ============================
+
+@export var hitstun: int = 12
+
+# ============================
+# EMPURRÃO
+# Intensidade do knockback.
+# ============================
+
+@export var pushback: float = 80.0
+
+# ============================
+# LANÇAMENTO
+# Para golpes que levantam
+# o oponente do chão.
+# (Não usaremos ainda.)
+# ============================
+
+@export var launch: Vector2 = Vector2.ZERO
+
+# ============================
+# ANIMAÇÃO
+# Nome da animação que será
+# executada no oponente.
+# ============================
+
+@export var hurt_animation: String = "Hurt"
+
+# ============================
+# DURAÇÃO DA HITBOX
+# Quantos frames a hitbox
+# permanece ativa.
+# (Usaremos nos ataques.)
+# ============================
+
+@export var active_frames: int = 3
