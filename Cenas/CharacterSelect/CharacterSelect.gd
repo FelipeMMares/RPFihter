86d6fc900949f185bcd645c73f4476d209799884
Felipe Mares <felipemmares@gmail.com>
2026-08-07 18:00:54 -0300

    criando a tela de seleção de personagens animada

--- Cenas/CharacterSelect/CharacterSelect.gd
+++ Cenas/CharacterSelect/CharacterSelect.gd
@@ -8,16 +8,50 @@ var fight_scene_path: String = (
 
 
 @onready var chun_li_button: Button = (
-	$HBoxContainer/ChunLiButton
+	$CenterContainer/VBoxContainer/CharacterGrid/ChunLiButton
 )
 
 @onready var elena_button: Button = (
-	$HBoxContainer/ElenaButton
+	$CenterContainer/VBoxContainer/CharacterGrid/ElenaButton
+)
+
+@onready var locked_button_1: Button = (
+	$CenterContainer/VBoxContainer/CharacterGrid/LockedButton1
+)
+
+@onready var locked_button_2: Button = (
+	$CenterContainer/VBoxContainer/CharacterGrid/LockedButton2
+)
+
+@onready var locked_button_3: Button = (
+	$CenterContainer/VBoxContainer/CharacterGrid/LockedButton3
+)
+
+@onready var locked_button_4: Button = (
+	$CenterContainer/VBoxContainer/CharacterGrid/LockedButton4
+)
+
+@onready var locked_button_5: Button = (
+	$CenterContainer/VBoxContainer/CharacterGrid/LockedButton5
+)
+
+@onready var locked_button_6: Button = (
+	$CenterContainer/VBoxContainer/CharacterGrid/LockedButton6
+)
+
+@onready var update_message: Label = (
+	$CenterContainer/VBoxContainer/UpdateMessage
+)
+
+@onready var message_timer: Timer = (
+	$MessageTimer
 )
 
 
 func _ready() -> void:
 	get_tree().paused = false
+
+	update_message.visible = false
 
 	chun_li_button.pressed.connect(
 		_on_chun_li_pressed
@@ -25,6 +59,34 @@ func _ready() -> void:
 
 	elena_button.pressed.connect(
 		_on_elena_pressed
+	)
+
+	locked_button_1.pressed.connect(
+		_on_locked_character_pressed
+	)
+
+	locked_button_2.pressed.connect(
+		_on_locked_character_pressed
+	)
+
+	locked_button_3.pressed.connect(
+		_on_locked_character_pressed
+	)
+
+	locked_button_4.pressed.connect(
+		_on_locked_character_pressed
+	)
+
+	locked_button_5.pressed.connect(
+		_on_locked_character_pressed
+	)
+
+	locked_button_6.pressed.connect(
+		_on_locked_character_pressed
+	)
+
+	message_timer.timeout.connect(
+		_on_message_timer_timeout
 	)
 
 	chun_li_button.grab_focus()
@@ -42,9 +104,25 @@ func _on_elena_pressed() -> void:
 	_start_fight()
 
 
+func _on_locked_character_pressed() -> void:
+	update_message.text = (
+		"AGUARDE A ATUALIZAÇÃO"
+	)
+
+	update_message.visible = true
+
+	message_timer.start()
+
+
+func _on_message_timer_timeout() -> void:
+	update_message.visible = false
+
+
 func _start_fight() -> void:
-	var error := get_tree().change_scene_to_file(
-		fight_scene_path
+	var error: Error = (
+		get_tree().change_scene_to_file(
+			fight_scene_path
+		)
 	)
 
 	if error != OK:
